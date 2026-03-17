358b02225dcb45c302ab3adbac5df788ad720cf6
Amichai Buskila <amichai.buskila@gmail.com>
2026-03-17 14:49:15 +0200

    aa

--- src/main/java/frc/robot/Subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/Subsystems/Vision/Vision.java
@@ -1,33 +1,40 @@
 package frc.robot.Subsystems.Vision;
 
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import limelight.Limelight;
 import limelight.networktables.LimelightResults;
 import limelight.networktables.target.pipeline.NeuralClassifier;
 
 public class Vision extends SubsystemBase {
+    Limelight limelight = new Limelight("limelight");
 
     public Vision() {
-        Limelight limelight = new Limelight("limelight");
-        // Get the results
-        limelight.getLatestResults().ifPresent((LimelightResults result) -> {
-            for (NeuralClassifier object : result.targets_Classifier) {
-                // Classifier says its a coral.
-                if (object.className.equals("coral")) {
-                    // Check pixel location of coral.
-                    if (object.ty > 2 && object.ty < 1) {
-                        // Coral is valid! do stuff!
-                    }
-                }
-            }
-        });
-
     }
 
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
+        limelight.getLatestResults().ifPresent((LimelightResults result) -> {
+            for (NeuralClassifier object : result.targets_Classifier) {
+                // Classifier says its a hub.
+                if (object.className.equals("hub")) {
+                    // Check pixel location of hub.
+                    if (object.ty < 2 && object.ty > 1) {
+                        double angle = getTx();
+
+                    }
+                }
+            }
+        });
     }
+public double getTx() {
+    return limelight.getLatestResults().map(result -> result.targets_Classifier[0].tx).orElse(0.0);
+}
+
+public double getTy() {
+    return limelight.getLatestResults().map(result -> result.targets_Classifier[0].ty).orElse(0.0);
+
+}
+
+
 
 }
