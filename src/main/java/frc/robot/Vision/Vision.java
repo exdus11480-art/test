0a0f9a5269e824463829e28278d631722474d277
exsodus <exdus11480@gmail.com>
2026-03-17 17:05:30 +0200

    Add Vision subsystem for Limelight integration
    - Created Vision class extending SubsystemBase
    - Integrated Limelight for basic targeting data retrieval
    - Implemented methods to get horizontal and vertical offsets, target area, and target validity
    - Set initial pipeline index for Limelight

---
+++ src/main/java/frc/robot/Vision/Vision.java
@@ -0,0 +1,25 @@
+package frc.robot.Vision;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import limelight.Limelight;
+import limelight.networktables.LimelightResults;
+import limelight.networktables.target.pipeline.NeuralClassifier;
+
+public class Vision extends SubsystemBase {
+    Limelight limelight = new Limelight("limelight");
+
+    public Vision() {
+// Basic targeting data
+double tx = LimelightHelpers.getTX("");  // Horizontal offset from crosshair to target in degrees
+double ty = LimelightHelpers.getTY("");  // Vertical offset from crosshair to target in degrees
+double ta = LimelightHelpers.getTA("");  // Target area (0% to 100% of image)
+boolean hasTarget = LimelightHelpers.getTV(""); // Do you have a valid target?
+
+double txnc = LimelightHelpers.getTXNC("");  // Horizontal offset from principal pixel/point to target in degrees
+double tync = LimelightHelpers.getTYNC("");  // Vertical offset from principal pixel/point to target in degrees
+
+LimelightHelpers.setPipelineIndex("", 0);
+    }
+
+
+}
